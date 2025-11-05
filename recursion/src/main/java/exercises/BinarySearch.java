054f3a0917759c6bb768a5bcfe15bb0b1324db4c
DJCoder311 <166066300+DJCoder311@users.noreply.github.com>
2025-11-04 19:25:23 -0500

    Completed Assignment

--- recursion/src/main/java/exercises/BinarySearch.java
+++ recursion/src/main/java/exercises/BinarySearch.java
@@ -33,16 +33,18 @@ public class BinarySearch {
         return search(a, target, 0, a.length - 1);
     }
 
-    // TODO: Implement recursively. DO NOT convert to a loop.
     private static int search(int[] a, int target, int lo, int hi) {
+        int mid = lo+((hi-lo)/2);
         // Base case 1: empty range
+        if(a.length == 0||lo>hi){
+            return -1;
+        }
         // Base case 2: found at mid
-        // Recursive case: pick left or right half
-        // Hints:
-        //   int mid = lo + (hi - lo) / 2;
-        //   if (target < a[mid]) return search(a, target, lo, mid - 1);
-        //   else if (target > a[mid]) return search(a, target, mid + 1, hi);
-        //   else return mid;
-        return -2; // TEMP: replace with your recursive solution
+        if(mid == target){
+            return mid;
+        }
+        if (target < a[mid]) return search(a, target, lo, mid - 1);
+        else if (target > a[mid]) return search(a, target, mid + 1, hi);
+        else return mid;
     }
 }
